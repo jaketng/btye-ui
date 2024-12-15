ba781f743552e327a866de92f0cf1d6a0d8f79d3
Jake Bowden <jakebowden@Jakes-MacBook-Pro-2.local>
2024-12-15 16:04:24 -0500

    Fixed module bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")], // Enable DaisyUI
+  plugins: [daisyui], // Use ES module import for DaisyUI
   daisyui: {
     themes: ["winter"], // Set the theme to 'winter'
   },
